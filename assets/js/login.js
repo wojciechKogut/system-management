014462f7cace4f36e43cd08b8c1cdc85d309020e
wojtek <wkgt33@gmail.com>
2019-01-20 18:35:42 +0100

    adjust login styles

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,6 @@
 const $ = require('jquery');
+require('../../public/bootstrap/css/bootstrap.css');
+require('../../public/fontawesome-free/css/all.min.css')
 require('../css/login.css');
 require('./login/validation/validation');
 
